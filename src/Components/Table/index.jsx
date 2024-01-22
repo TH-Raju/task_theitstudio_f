14d0acbfb56d95ea8854022279cbf30f69b0a9a4
Tofajjol Hosen Raju <rajukhan894200@gmail.com>
2024-01-22 11:06:59 +0600

    option added

--- src/Components/Table/index.jsx
+++ src/Components/Table/index.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import toast from "react-hot-toast";
 
 const Table = () => {
+  const handleDeleteUser = (task) => {
+    console.log("Delete", task);
+    // fetch(
+    //   `https://task-manage-9e14yw343-th-raju.vercel.app/api/v1/task/${task._id}`,
+    //   {
+    //     method: "DELETE",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // )
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     if (data.success) {
+    //       toast.success("Task Deleted successfully");
+    //       refetch();
+    //     }
+    //   });
+  };
   return (
     <div>
       <h1>Table Data</h1>
-      <div className="overflow-x-auto">
+      <div className="overflow-x-auto pb-20">
         <table className="table">
           {/* head */}
           <thead>
@@ -37,7 +57,38 @@ const Table = () => {
               <td>gamil</td>
               <td>Coming</td>
               <th>
-                <button className="btn btn-ghost btn-xs">update/delete</button>
+                <div className="border border-blue-700 rounded-lg">
+                  <div className="dropdown ">
+                    <label tabIndex={0} className="btn m-1 btn-xs">
+                      option
+                    </label>
+                    <ul
+                      tabIndex={0}
+                      className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-32"
+                    >
+                      <li>
+                        <button
+                          className="btn btn-primary btn-sm"
+                          // onClick={() => handleMakeUser(task._id)}
+                          //   onClick={() =>
+                          //     document.getElementById(task._id).showModal()
+                          //   }
+                        >
+                          Edit
+                        </button>
+                      </li>
+                      <li>
+                        <label
+                          onClick={() => handleDeleteUser("task")}
+                          htmlFor="confirmation-modal"
+                          className="btn bg-red-600 mt-1 hover:bg-red-900 text-white  btn-sm "
+                        >
+                          Delete
+                        </label>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </th>
             </tr>
           </tbody>
